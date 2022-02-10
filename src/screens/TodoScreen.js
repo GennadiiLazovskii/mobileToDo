86680149abf39996457fef3b8cb52bdb4f71289d
Gena Lazovskiy <genalazovskiy@MacBook-Pro-Gena.local>
2022-02-10 15:56:37 +0200

    Add style button

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,16 +1,34 @@
 import { StyleSheet, View, Text, Button } from "react-native";
+import THEME from '../theme';
 
-const TodoScreen = ({goBack, todo}) => {
+const TodoScreen = ({ goBack, todo }) => {
     return (
         <View>
             <Text>{todo.title}</Text>
-            <Button title="Back" onPress={goBack}/>
+            <View style={styles.buttons}>
+                <View style={styles.button}>
+                    <Button title="Back" color={THEME.GREY_COLOR} onPress={goBack} />
+                </View>
+                <View style={styles.button}>
+                    <Button
+                        title="Remove"
+                        color={THEME.DANGER_COLOR}
+                        onPress={() => console.log("To remove")}
+                    />
+                </View>
+            </View>
         </View>
-    )
-}
+    );
+};
 
 const styles = StyleSheet.create({
-
-})
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    button: {
+        width: '40%'
+    }
+});
 
 export default TodoScreen;
---
+++ src/theme.js
@@ -0,0 +1,7 @@
+const THEME = {
+    DANGER_COLOR: '#e53935',
+    GREY_COLOR: '#757575',
+    MAIN_COLOR: '#3949ab'
+}
+
+export default THEME;
